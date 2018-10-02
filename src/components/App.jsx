d65547310ffbde073430c10acaf7f50722f5a90c
Elbehary <ahmedelbehary0@gmail.com>
2018-10-02 12:08:20 +0200

    frist commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header tagline="This could be your shopping list." />
+        <ToDoList />
+      </div>
+    );
+  }
+}
+
+export default App;
