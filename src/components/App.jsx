e7381a34f38e11aed4286cecf965dbb52a3ec882
Elbehary <ahmedelbehary0@gmail.com>
2018-10-02 14:39:33 +0200

    create from and helpers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
+import ToDoForm from "./ToDoForm";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header tagline="This could be your shopping list." />
+        <Header tagline="Here are all the next tasks." />
+        <ToDoForm />
         <ToDoList />
       </div>
     );
---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ToDoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  textInput = React.createRef();
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(`Creat new item: ${this.textInput.current.value}`);
+  }
+
+  render() {
+    return (
+      <form className="input-group my-3" onSubmit={this.handleSubmit}>
+        <input
+          className="form-control"
+          name="name"
+          type="text"
+          placeholder="Add a new to-do item ..."
+          ref={this.textInput}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="submit">
+            <i className="fa fa-plus" aria-hidden="true" />
+            &nbsp;Add item
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ToDoForm;
