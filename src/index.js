742cf85d169c95f5acd954d5279d0d9a8062a502
Elbehary <ahmedelbehary0@gmail.com>
2018-10-16 15:58:25 +0200

    intgrate redux to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+// React
 import React from "react";
 import { render } from "react-dom";
+
+// Redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import appReducer from "./reducers/index";
+
+// Components
 import Router from "./components/Router";
 
+// Styles
 import "font-awesome/css/font-awesome.css";
 import "./css/style.css";
 
-render(<Router />, document.getElementById("root"));
+// Create Redux store and initialize the app
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+render(
+  <Provider store={store}>
+    <Router />
+  </Provider>,
+  document.querySelector("#main")
+);
